1c0b583cca0b5f2dde823ed630cba5420adb7955
kcavs <kcav1321@gmail.com>
2024-02-07 14:40:50 -0500

    page 9!!

---
+++ page_9.java
@@ -0,0 +1,14 @@
+/*** 
+ * project: page.9
+ * programer: kiersten cavalli
+ * date: february 7th, 2023
+ * program: Page9.java
+********/
+public class page_9
+{
+    public static void main (String[]args)
+    {
+        System.out.println("hi");
+        System.out.println("LabOneA");
+    }
+}
